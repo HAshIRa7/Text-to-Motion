efc83671e8a35f9a0fb78f6732cf93f928051d3e
Egor <maslennikov.em@phystech.edu>
2026-04-11 20:55:08 +0000

    dataset for robot motions

---
+++ src/text_to_motion/dataset.py
@@ -0,0 +1,48 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+from .utils import collect_data
+import bisect
+
+
+class HumanoidDataset(Dataset):
+    def __init__(self, motions_folder: str, motions_len: int = 500):
+        self.data_dict = collect_data(motions_folder)
+        self.idx2motion = {}
+        for idx, motion_file in enumerate(self.data_dict):
+            self.idx2motion[idx] = motion_file
+        
+        self.motions_len = motions_len
+        self.motions_len_cumsum = []
+        
+        total_len = 0
+        for motion_file in self.data_dict:
+            motion_len = self.data_dict[motion_file]['lin_vel'].shape[0]
+            total_len += (motion_len - self.motions_len)
+            self.motions_len_cumsum.append(total_len)
+        self.total_len = total_len
+    
+    def __len__(self,):
+        return self.total_len - self.motions_len
+    
+    def __getitem__(self, idx):
+        motion_index = bisect.bisect_right(self.motions_len_cumsum, idx)
+        if motion_index == 0:
+            index_in_motion = idx
+        else:
+            index_in_motion = idx - self.motions_len_cumsum[motion_index - 1]
+        
+        motion = self.data_dict[self.idx2motion[motion_index]]
+        joint_pos = motion['joint_pos'][index_in_motion:index_in_motion + self.motions_len]
+        roll = motion['roll'][index_in_motion:index_in_motion + self.motions_len]
+        pitch = motion['pitch'][index_in_motion:index_in_motion + self.motions_len]
+        lin_vel = motion['lin_vel'][index_in_motion:index_in_motion + self.motions_len]
+        ang_vel = motion['ang_vel'][index_in_motion:index_in_motion + self.motions_len]
+        
+        return torch.cat([
+            torch.tensor(joint_pos).to(dtype=torch.float32),
+            torch.tensor(roll[:, None]).to(dtype=torch.float32),
+            torch.tensor(pitch[:, None]).to(dtype=torch.float32),
+            torch.tensor(lin_vel).to(dtype=torch.float32),
+            torch.tensor(ang_vel[:, None]).to(dtype=torch.float32),
+        ], dim=-1)
